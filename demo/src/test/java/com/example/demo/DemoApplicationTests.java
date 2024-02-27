2de0ac600c864dc49d7da027329e48eb63fe5a01
Akash Senthilkumar <akashsenthilkumar520@gmail.com>
2024-02-27 06:05:11 +0100

    test

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-public class CalculatriceTest {
+public class DemoApplicationTests {
 
     @Test
     public void testAdd() {
