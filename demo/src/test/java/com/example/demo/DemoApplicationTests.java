2a015c5ad03702ce14f256131cbe51d74c59b3c1
Akash Senthilkumar <akashsenthilkumar520@gmail.com>
2024-02-27 05:20:30 +0100

    test

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
+public class CalculatriceTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void testAdd() {
+        assertEquals(3, Calculatrice.add(1, 2));
+    }
 
+    @Test
+    public void testMulti() {
+        assertEquals(6, Calculatrice.multi(2, 3));
+    }
+
+    @Test
+    public void testDiv() {
+        assertEquals(2, Calculatrice.div(4, 2));
+    }
 }
